5fd49795d910b34e9bdb733ca993f4d5ede55a11
Stefano Pedroli <stefanopedroli@imac-di-stefano.local>
2017-11-06 13:46:52 +0100

    Catch viwewillDisappear

--- MyGarageApp/Side Bar View Class/WebKitViewController.swift
+++ MyGarageApp/Side Bar View Class/WebKitViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import WebKit
+import Photos
 
 class WebKitViewController: UIViewController, WKUIDelegate {
         
@@ -23,19 +24,53 @@ class WebKitViewController: UIViewController, WKUIDelegate {
         override func viewDidLoad() {
             super.viewDidLoad()
             
+            let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: Selector(("longPressed:")))
+            longPressRecognizer.minimumPressDuration = 0.5
+            webView.addGestureRecognizer(longPressRecognizer)
+            
             let myURL = URL(string: "https://www.google.it/imghp?hl=it")
             let myRequest = URLRequest(url: myURL!)
             webView.load(myRequest)
         }
-
-    /*
+    
+    
+    func webViewDidClose(_ webView: WKWebView) {
+        self.performSegue(withIdentifier: "unwindToMenu", sender: self)
+    }
+    
+    func longPressed(sender: UILongPressGestureRecognizer) {
+        print("Save img")
+        //UIImageWriteToSavedPhotosAlbum(imageview.image, self, "image:didFinishSavingWithError:contextInfo:", nil)
+    }
+    
+    func image(image: UIImage!, didFinishSavingWithError error: NSError!, contextInfo: AnyObject!) {
+        if (error != nil) {
+            // Something wrong happened.
+        } else {
+            // Everything is alright.
+        }
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        print("sono entrato nell'alert di javascript")
+    }
+    
+    func completionFunction(param: AnyObject)
+    {
+        print("entrato in copletion Function!")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main",bundle: nil)
+        mainStoryboard.instantiateViewController(withIdentifier: "listOfCars")
+    }
     // MARK: - Navigation
-
+    /*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
+        self.performSegue(withIdentifier: "unwindToMenu", sender: self)
+    }*/
 
 }
